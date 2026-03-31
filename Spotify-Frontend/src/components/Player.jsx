0f52eda52682f87c5afe6962091416a0b9491b49
ChalakantiPrudhvi <chalakantiprudhvi1234@gmail.com>
2026-03-31 22:16:34 +0530

    pages updation

--- Spotify-Frontend/src/components/Player.jsx
+++ Spotify-Frontend/src/components/Player.jsx
@@ -3,7 +3,7 @@ import { assets,songsData } from '../assets/frontend-assets/assets'
 import { Playercontext } from '../context/Playercontext'
 const Player = () => {
 
-  const {track,seekbar,seekbg,playStatus,play,pause,time} = useContext(Playercontext);
+  const {track,seekbar,seekbg,playStatus,play,pause,time,prev,Next,seeksong} = useContext(Playercontext);
   return (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
         <div className='hidden lg:flex items-center gap-4'>
@@ -16,17 +16,17 @@ const Player = () => {
       <div className='flex flex-col items-center gap-1 m-auto'>
           <div className='flex gap-4'>
             <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
-            <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
+            <img onClick={prev} className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
             {playStatus?
             <img onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon} alt="" />:
             <img onClick={play} className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
              }
-            <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" /> 
+            <img onClick={Next} className='w-4 cursor-pointer' src={assets.next_icon} alt="" /> 
             <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />         
         </div>
         <div className='flex items-center gap-5'>
             <p>{time.currentTime.minute}:{time.currentTime.second}</p>
-            <div ref={seekbg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+            <div ref={seekbg} onClick={seeksong} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
                 <hr ref={seekbar} className='h-1 border-none w-0 bg-green-800 rounded-full'/>
             </div>
             <p>{time.totalTime.minute}:{time.totalTime.second}</p>
--- Spotify-Frontend/src/context/Playercontext.jsx
+++ Spotify-Frontend/src/context/Playercontext.jsx
@@ -3,67 +3,127 @@ import { songsData } from "../assets/frontend-assets/assets";
 
 export const Playercontext = createContext();
 
-const PlayercontextProvider = (props) =>{
+const PlayercontextProvider = (props) => {
+  const audioRef = useRef();
+  const seekbg = useRef();
+  const seekbar = useRef();
 
-     const audioRef = useRef();
-     const seekbg = useRef();
-     const seekbar = useRef();
-
-const [track,settrack] = useState(songsData[0]);
-const [playStatus,setPlayerstatus] = useState(false);
-const [time,settime] = useState({
-    currentTime:{
-        second:0,
-        minute:0
+  const [track, settrack] = useState(songsData[0]);
+  const [playStatus, setPlayerstatus] = useState(false);
+  const [time, settime] = useState({
+    currentTime: {
+      second: 0,
+      minute: 0,
     },
-    totalTime:{
-        second:0,
-        minute:0
-    }
-}); 
+    totalTime: {
+      second: 0,
+      minute: 0,
+    },
+  });
 
-
-   const play=()=>{
+  const play = () => {
     audioRef.current.play();
     setPlayerstatus(true);
-   }
+  };
 
-   const pause=()=>{
+  const pause = () => {
     audioRef.current.pause();
     setPlayerstatus(false);
-   }
-useEffect(() => {
-  setTimeout(() => {
-    if (audioRef.current) {
+  };
 
-      audioRef.current.ontimeupdate = () => {
-        settime({
-          currentTime: {
-            second: Math.floor(audioRef.current.currentTime % 60),
-            minute: Math.floor(audioRef.current.currentTime / 60)
-          },
-          totalTime: {
-            second: Math.floor(audioRef.current.duration % 60),
-            minute: Math.floor(audioRef.current.duration / 60)
-          }
-        });
-      };
+//  const prev= async() =>{
+//   if(track.id>0){
+//     await settrack(track.id-1);
+//     await audioRef.current.play();
+//     setPlayerstatus(true);
+//   }
+//  }
+//  const Next= async() =>{
+//   if(track.id<songsData.length-1){
+//     await settrack(track.id+1);
+//     await audioRef.current.play();
+//     setPlayerstatus(true);
+//   }
+//  }
+const prev = () => {
+  if (track.id > 0) {
+    settrack(songsData[track.id - 1]);
+    audioRef.current.play();
+    setPlayerstatus(true);
+  }
+};
 
-    }
-  }, 1000);
-}, []);
-    const contextvalue={
-        audioRef,
-        seekbg,
-        seekbar,
-        track,settrack,
-        playStatus,setPlayerstatus,
-        time,settime,play,pause
-    }
-    return(
-        <Playercontext.Provider value={contextvalue}>
-            {props.children}
-        </Playercontext.Provider>
-    )
-}
+const Next = () => {
+  if (track.id < songsData.length - 1) {
+    settrack(songsData[track.id + 1]);
+    audioRef.current.play();
+    setPlayerstatus(true);
+  }
+};
+// useEffect(() => {
+//   if (!audioRef.current) return;
+
+//   const audio = audioRef.current;
+
+//   const playAudio = async () => {
+//     try {
+//       await audio.play();
+//       setPlayerstatus(true);
+//     } catch (err) {
+//       console.log("Play error:", err);
+//     }
+//   };
+
+//   playAudio();
+// }, [track]);
+  const playwithId=async(id)=>{
+    await settrack(songsData[id]);
+    await audioRef.current.play();
+    setPlayerstatus(true);
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.ontimeupdate = () => {
+          seekbar.current.style.width=(Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
+          settime({
+            currentTime: {
+              second: Math.floor(audioRef.current.currentTime % 60),
+              minute: Math.floor(audioRef.current.currentTime / 60),
+            },
+            totalTime: {
+              second: Math.floor(audioRef.current.duration % 60),
+              minute: Math.floor(audioRef.current.duration / 60),
+            },
+          });
+        };
+      }
+    }, 1000);
+  }, []);
+
+
+
+  const seeksong = async(e)=>{
+      audioRef.current.currentTime=((e.nativeEvent.offsetX/seekbg.current.offsetWidth)*audioRef.current.duration)
+  }
+  const contextvalue = {
+    audioRef,
+    seekbg,
+    seekbar,
+    track,
+    settrack,
+    playStatus,
+    setPlayerstatus,
+    time,
+    settime,
+    play,
+    pause,
+    playwithId,prev,Next,seeksong
+  };
+  return (
+    <Playercontext.Provider value={contextvalue}>
+      {props.children}
+    </Playercontext.Provider>
+  );
+};
 export default PlayercontextProvider;
